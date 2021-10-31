a511749365fa00e4146255b01205e09b891ef107
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-10-31 17:39:29 +0000

    http interceptor added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { httpInterceptorProviders } from './_httpInterceptors';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    //httpInterceptorProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
